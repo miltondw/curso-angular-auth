18809e511360c4e24142fa22e2d8905dea96db21
miltondw <estradamilton2001@gmail.com>
2023-09-08 15:21:13 -0500

    implementación del registro, login, password recovery y el token con su guardian

--- src/app/modules/auth/components/recovery-form/recovery-form.component.ts
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 import { CustomValidators } from '@utils/validators';
+import { AuthService } from '@services/auth.service';
+import { RequestStatus } from '@models/request-status.model';
 
 @Component({
   selector: 'app-recovery-form',
   templateUrl: './recovery-form.component.html',
 })
+
 export class RecoveryFormComponent {
+
   form = this.formBuilder.nonNullable.group(
     {
       newPassword: ['', [Validators.minLength(6), Validators.required]],
@@ -25,11 +30,39 @@ export class RecoveryFormComponent {
   faEyeSlash = faEyeSlash;
   showPassword = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+   token = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.queryParamMap.subscribe(params => {
+      const token = params.get('token');
+      if (token) {
+        this.token = token;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
 
   recovery() {
     if (this.form.valid) {
-      // Todo
+         const { newPassword } = this.form.getRawValue();
+      this.status = 'loading';
+      this.authService.changePassword(this.token, newPassword)
+      .subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/login']);
+        },
+        error: () => {
+          this.status = 'failed';
+        }
+      });
     } else {
       this.form.markAllAsTouched();
     }
